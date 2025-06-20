ad627ae3e05b46ffe3b02c7f921bc7958bc64d9e
isadoraAlexande <isadoraalexandre489@gmail.com>
2025-06-20 15:35:22 -0300

    tirando comentario do f

--- src/Gerenciador.cpp
+++ src/Gerenciador.cpp
@@ -112,7 +112,7 @@ void Gerenciador::comandos(Grafo* grafo) {
 
                 vector<char> ids = get_conjunto_ids(grafo,tam);
                 Grafo* arvore_geradora_minima_kruskal = grafo->arvore_geradora_minima_kruskal(ids);
-                //cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
+                cout<<"Metodo de impressao em tela nao implementado"<<endl<<endl;
 
                 if(pergunta_imprimir_arquivo("agm_kruskal.txt")) {
                     cout<<"Metodo de impressao em arquivo nao implementado"<<endl;
