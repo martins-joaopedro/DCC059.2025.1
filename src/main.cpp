123b45d28396adef13a77460ece386cfaedf6f26
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2025-07-26 11:45:31 -0300

    faz correções nos arquivos txt

--- src/main.cpp
+++ src/main.cpp
@@ -211,7 +211,7 @@ vector<char> updates_LC(map<char, No*>& mapa_nos, vector<char> LC, vector<char> 
 // algoritmo adaptativo guloso randomizado
 vector<char> randomized_adaptative_greedy(Grafo* grafo, float alfa) {
     
-    bool debug = false;
+    bool debug = true;
     map<char, No*> mapa_nos;
     vector<char> LC;
 
@@ -324,7 +324,7 @@ bool check_validity(vector<char> S, Grafo* grafo) {
 void run(Grafo* grafo) {
     map<float, vector<int>> sols;
     float alphas[] = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6};
-    int MAX_IT = 10;
+    int MAX_IT = 1;
     srand(time(0));
 
     for (auto alpha : alphas) {
